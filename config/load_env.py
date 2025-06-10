4b791e079f0f6e8b72d5ec5a0693d8050595977a
Zayan <zayan.qazi.mail@gmail.com>
2025-06-10 13:57:08 -0500

    load_env.py done

--- config/load_env.py
+++ config/load_env.py
@@ -2,9 +2,11 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
-def load_env_vars():
+DEFAULT_PATH = Path.home() / ".secrets/.llm_apis"
+
+def load_env_vars(path=DEFAULT_PATH):
     # load ~/.secrets/.llm_apis
-    load_dotenv(dotenv_path=Path.home() / ".secrets/.llm_apis")
+    load_dotenv(dotenv_path=path)
 
     # load project .env
     load_dotenv()
@@ -14,3 +16,4 @@ if __name__=="__main__":
     print("OPENROUTER_API_KEY", os.getenv("OPENROUTER_API_KEY"))
     print("OPENAI_API_KEY", os.getenv("OPENAI_API_KEY"))
     print("LANGSMITH_API_KEY", os.getenv("LANGSMITH_API_KEY"))
+    print("LANGSMITH PROJECT NAME", os.getenv("LANGSMITH_PROJECT"))
